feec6b18e96e73418007562f796b8939655e99f4
matt <javascript.12.03.2024@gmail.com>
2025-01-29 15:38:25 +0000

    Update console.js

--- xs-js/js/console/console.js
+++ xs-js/js/console/console.js
@@ -3,6 +3,15 @@
 
 
         var console             = {};
+        globalThis.console      = console;
+        
         console.log             = function(){trace([...arguments].join(' ')+'\n')};
+        console.error           = function(){console.log.apply(console,['[ERROR]',...arguments])};
+        console.warn            = function(){console.log.apply(console,['[ WARN]',...arguments])};
+        console.clear           = ()=>{};
         console.json            = v=>console.log(JSON.stringify(v,null,4));
         
+        Object.defineProperty(console,'date',{get:()=>console.log(new Date().toUTCString())});
+
+
+
