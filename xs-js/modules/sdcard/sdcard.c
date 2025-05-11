2074ceabe84faf7c70fa2ade16e5c8ed2a73a43e
matt <javascript.12.03.2024@gmail.com>
2025-05-11 07:07:43 +0100

    save text

--- xs-js/modules/sdcard/sdcard.c
+++ xs-js/modules/sdcard/sdcard.c
@@ -213,8 +213,13 @@
         void xs_sd_destructor ( void *data ) { }
         
         void xs_sd_mount ( xsMachine *the ) {
-            esp_err_t ret; xsmcVars(1);
         
+            esp_err_t ret; 
+            
+/*        
+
+            xsmcVars(1);
+
             int miso = PIN_NUM_MISO;
             int mosi = PIN_NUM_MOSI;
             int clk  = PIN_NUM_CLK;
@@ -258,6 +263,85 @@
             	xsmcGet(xsVar(0), xsArg(0), xsID_max_files);
             	max_files = xsmcToInteger(xsVar(0));
             }
+*/        
+        
+        
+        
+        
+        
+              esp_err_t result;
+                                                                                ESP_LOGI(TAG, "Initializing SD card");
+                                                                                
+                                                                                // Use settings defined above to initialize SD card and mount FAT filesystem.
+                                                                                // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+                                                                                // Please check its source code and implement error recovery when developing
+                                                                                // production applications.
+                                                                                ESP_LOGI(TAG, "Using SPI peripheral");
+                                                                                
+                                                                                // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+                                                                                // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+                                                                                // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+              sdmmc_host_t host   = SDSPI_HOST_DEFAULT();
+                                                                                // This initializes the slot without card detect (CD) and write protect (WP) signals.
+                                                                                // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+              spi_bus_config_t bus_cfg    = {
+                    .mosi_io_num        = mosi,
+                    .miso_io_num        = miso,
+                    .sclk_io_num        = clk,
+                    .quadwp_io_num      = -1,
+                    .quadhd_io_num      = -1,
+                    .max_transfer_sz    = 4000,
+              };
+              
+              result    = spi_bus_initialize(host.slot,&bus_cfg,SDSPI_DEFAULT_DMA);
+              if(result!=ESP_OK){
+                                                                                ESP_LOGE(TAG, "Failed to initialize bus.");
+                    return result;
+              }
+              
+                                                                                // Options for mounting the filesystem.
+                                                                                // If format_if_mount_failed is set to true, SD card will be partitioned and
+                                                                                // formatted in case when mounting fails.
+              esp_vfs_fat_sdmmc_mount_config_t mount_config   = {
+                    .format_if_mount_failed   = false,
+                    .max_files                = 5,
+                    .allocation_unit_size     = 16*1024
+              };
+              
+              sdspi_device_config_t slot_config   = SDSPI_DEVICE_CONFIG_DEFAULT();
+              slot_config.gpio_cs                 = cs;
+              slot_config.host_id                 = host.slot;
+              sdmmc_card_t* card;
+                                                                                ESP_LOGI(TAG, "Mounting filesystem");
+              result    = esp_vfs_fat_sdspi_mount(mount,&host,&slot_config,&mount_config,&card);
+              if(result!=ESP_OK){
+                    if(result==ESP_FAIL){
+                                                                                ESP_LOGE(TAG, "Failed to mount filesystem. ");
+                    }else{
+                                                                                ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                                                                                         "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(result));
+                    }
+                    return result;
+              }
+                                                                                ESP_LOGI(TAG, "Filesystem mounted");
+/*
+              if(debug){
+                                                                                // Card has been initialized, print its properties
+                    //sdmmc_card_print_info(stdout,card);
+              }
+*/
+
+
+              list_add(mount,&host,card);
+              
+              //return ESP_OK;
+              
+        
+        
+        
+        
+        
+        
         
                 /*
             // Options for mounting the filesystem.
