24c881183c3ac676ec9222dba61b305e36359c32
matt <javascript.12.03.2024@gmail.com>
2025-05-25 14:46:24 +0100

    Create console.js

---
+++ xs-js/code/sdcard/sdcard-test/console/console.js
@@ -0,0 +1,42 @@
+
+
+
+/*
+//:
+
+//console:d
+
+29-01-25
+
+*/
+
+        var console             = {}
+        globalThis.console      = console;
+        export default console;
+        
+        
+        Object.defineProperty(console,'date',{get:()=>console.log(new Date().toLocaleString())})
+        
+        console.fn              = [trace]
+        console.fn.add          = fn=>!console.fn.includes(fn) && console.fn.push(fn)
+        console.fn.rem          = fn=>{var i=console.fn.indexOf(fn);i!=-1 && console.fn.splice(i,1)}
+        
+        console.log             = function(){console.write.apply(null,[...arguments,'\n'])};
+        console.ok              = function(){console.log.apply(null,['[    ok ]',...arguments])}
+        console.error           = function(){console.log.apply(null,['[ ERROR ]',...arguments])}
+        console.warn            = function(){console.log.apply(null,['[  WARN ]',...arguments])}
+        console.clear           = ()=>{};
+        console.json            = function(v){console.log(JSON.stringify(v,null,4))}
+        
+        console.write           = function(){
+        
+                                        var str   = [...arguments].join(' ');
+                                        console.fn.forEach(fn=>fn(str));
+                                        
+                                  }//write
+                                  
+                                  
+                                  
+                                  
+                                  
+                                  
